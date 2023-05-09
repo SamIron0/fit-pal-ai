dae7ed79d695f613e00ecb68265431f4a09d6e7f
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-05-09 20:58:35 +0000

    test-commit

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -52,14 +52,14 @@ export default function Pricing({ products }: Props) {
         <div className="max-w-6xl mx-auto py-8 sm:py-24 px-4 sm:px-6 lg:px-8">
           <div className="sm:flex sm:flex-col sm:align-center"></div>
           <p className="text-6xl font-extrabold text-white sm:text-center sm:text-6xl">
-            No subscription pricing plans found. Create them in your{' '}
+            Your AI{' '}
             <a
               className="text-pink-500 underline"
               href="https://dashboard.stripe.com/products"
               rel="noopener noreferrer"
               target="_blank"
             >
-              Stripe Dashboard
+              Fitness Coach
             </a>
             .
           </p>
