8e5327cf088c9dd0450ba8200367c28c50a9cf2c
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-05-10 17:22:48 +0000

    EDIT HOME

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -10,25 +10,16 @@ export default function HomePage() {
             </div>
             <div>
                 <p className="mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl max-w-2xl m-auto">
-                    hkdhi idud iduiubd
+                    Ask it to make meal plans/workout plans
                 </p>
             </div>
         </div>
 
-        <div className="max-w-6xl mx-auto py-8 sm:py-24 px-4 sm:px-6 lg:px-8">
-            <div className="sm:flex sm:flex-col sm:align-center">
-                <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
-                    Your AI{' '}<p className="text-blue-500"> Fitness Coach. </p>
-                </h1>
+        <div className="bg-black">
+            <div className="bg-gray-800 rounded-lg border border-gray-300 w-60% mx-auto p-4">
+             F
             </div>
-            <div>
-                <p className="mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl max-w-2xl m-auto">
-                    hkdhi idud iduiubd
-                </p>
-            </div>
-        </div>
-        <div className="bg-gray-500">
-            f
+
         </div>
         
         <div>
