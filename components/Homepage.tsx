dc0f95113265a35d24f0023139efa70cb0f8364b
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-05-10 02:46:46 +0000

    edit homepage text

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -5,15 +5,23 @@ export default function HomePage() {
         <div className="max-w-6xl mx-auto py-8 sm:py-24 px-4 sm:px-6 lg:px-8">
             <div className="sm:flex sm:flex-col sm:align-center">
             <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
-                Pricing Plans
+                Your AI{' '}
+                <a
+                    className="text-pink-500 underline"
+                    href="https://dashboard.stripe.com/products"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    >
+                    Fitness Coach
+                    </a>
+                    .
             </h1>
             <p className="mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl max-w-2xl m-auto">
-                Start building for free, then add a site plan to go live. Account
-                plans unlock additional features.
+                
             </p>
             </div>
         </div>
-        
+
         <div>
             <p className="mt-24 text-xs uppercase text-zinc-400 text-center font-bold tracking-[0.3em]">
             Brought to you by
