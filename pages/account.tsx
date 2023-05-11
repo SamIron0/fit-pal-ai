24e084e2762c225d0e57eea9d877e5d92c6ae2ec
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-05-11 04:24:21 +0000

    test

--- pages/account.tsx
+++ pages/account.tsx
@@ -100,7 +100,7 @@ export default function Account({ user }: { user: User }) {
         <div className="sm:flex px-4 sm:flex-col sm:align-center">
           <div className="border border-zinc-700	max-w-3xl w-full p rounded-md m-auto">
             <div className="px-5">
-              <h3 className="text-xl mb-1 font-medium">Hello Samuel</h3>
+              <h3 className="text-xl my-3 font-medium">Hello Samuel</h3>
             </div>
           </div>
         </div>
