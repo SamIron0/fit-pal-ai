7a8361f8595b19aa39e4ffd7d49fe07031e8fb5d
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-05-11 03:33:07 +0000

    test

--- pages/account.tsx
+++ pages/account.tsx
@@ -31,6 +31,21 @@ function Card({ title, description, footer, children }: Props) {
       </div>
     </div>
   );
+} 
+
+function PlanCard({ title, description, footer, children }: Props) {
+  return (
+    <div className="border border-zinc-700	max-w-3xl w-full p rounded-md m-auto my-8">
+      <div className="px-5 py-4">
+        <h3 className="text-2xl mb-1 font-medium">{title}</h3>
+        <p className="text-zinc-300">{description}</p>
+        {children}
+      </div>
+      <div className="border-t border-zinc-700 bg-zinc-900 p-4 text-zinc-500 rounded-b-md">
+        {footer}
+      </div>
+    </div>
+  );
 }
 
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
@@ -93,6 +108,31 @@ export default function Account({ user }: { user: User }) {
         <div className="sm:flex px-4 sm:flex-col sm:align-center">
           <div className="border border-zinc-700	max-w-3xl w-full p rounded-md m-auto my-8">
             <div className="px-5 py-4">
+            <Card
+              title="Mondy"
+              description={
+                subscription
+                  ? `You are currently on the ${subscription?.prices?.products?.name} plan.`
+                  : ''
+              }
+              footer={
+                <div className="flex items-start justify-between flex-col sm:flex-row sm:items-center">
+                  
+                </div>
+              }
+              >
+                <div className="text-xl mt-8 mb-4 font-semibold">
+                  {isLoading ? (
+                    <div className="h-12 mb-6">
+                      <LoadingDots />
+                    </div>
+                  ) : subscription ? (
+                    `${subscriptionPrice}/${subscription?.prices?.interval}`
+                  ) : (
+                    <Link href="/">Select something</Link>
+                  )}
+                </div>
+          </Card>
             </div>
             <div className="border-t border-zinc-700 bg-zinc-900 p-4 text-zinc-500 rounded-b-md">
               <div>
