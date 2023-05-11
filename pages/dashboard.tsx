9517db32c58d6a77d9f578c05248b809f6eb9369
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-05-11 20:29:40 +0000

    d

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -13,7 +13,7 @@ export default function DashBoard() {
                         <input
                         type="text"
                         className="bg-gray-700 rounded-full py-2 px-4 text-gray-200 w-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
-                        placeholder="Make a workout plan for 4 days.."
+                        placeholder="Make a dworkout plan for 4 days.."
                         />
                         <button className="absolute right-0 top-0 h-full px-4 bg-gray-500 text-gray-100 rounded-r-full focus:outline-none hover:bg-gray-600">
                         Button
