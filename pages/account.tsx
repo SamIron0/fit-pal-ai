bc5655bcfed6ee706e14642b613f2dacbbf1e10c
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-05-11 04:54:56 +0000

    g

--- pages/account.tsx
+++ pages/account.tsx
@@ -100,14 +100,14 @@ export default function Account({ user }: { user: User }) {
         <div className="sm:flex px-4 sm:flex-col sm:align-center">
           <div className="border border-zinc-700	max-w-3xl w-full p rounded-md m-auto">
             <div className="px-5">
-              <h3 className="text-2xl mb-1 font-medium">Hello Samuel</h3>
+              <h3 className="text-xl my-1 font-medium">Hello Samuel</h3>
             </div>
           </div>
         </div>
 
         <div className="sm:flex px-4 sm:flex-col sm:align-center">
           <div className="border border-zinc-700	max-w-3xl w-full p rounded-md m-auto my-8">
-            <div className="px-5 py-4"> 
+            <div className="px-5"> 
               <div className="flex overflow-x-scroll space-x-4">
                 
                 <PlanCard
