097953141d976801be7dde74f632b74502f04217
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-05-15 06:25:57 +0000

    try

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -4,22 +4,23 @@ import { NextApiHandler } from 'next';
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
-  apiKey: "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",
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 // method to add two numbers
 
 const handler: NextApiHandler = async (req, res) => {
   if (req.method === 'GET') {
-    try {
+    //try {
       const meal_plan = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
         messages: [{ role: "user", content: "Make me a meal plan" }],
       });
+      window.alert(meal_plan.data.choices[0].message. stringify)
       res.status(200).json(meal_plan.data.choices[0].message);
-    } catch (error) {
-      if (error) return alert((error as Error).message);
-    }
+    //} catch (error) {
+      //if (error) return alert((error as Error).message);
+    //}
   } else {
     res.status(405).json({ message: 'Method Not Allowed' });
   }
