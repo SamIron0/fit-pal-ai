c1572f4b6b0e540d2f9a0c12eabdd82b1f735bf9
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-05-15 07:37:51 +0000

    try

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -10,17 +10,19 @@ const openai = new OpenAIApi(configuration);
 // method to add two numbers
 
 const handler: NextApiHandler = async (req, res) => {
+
   if (req.method === 'GET') {
-    //try {
+    try {
       const meal_plan = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
         messages: [{ role: "user", content: "Make me a meal plan" }],
       });
-      window.alert(meal_plan.data.choices[0].message.stringify)
-      res.status(200).json(meal_plan.data.choices[0].message);
-    //} catch (error) {
-      //if (error) return alert((error as Error).message);
-    //}
+      //window.alert(meal_plan.data.choices[0].message.stringify)
+      //res.status(200).json(meal_plan.data.choices[0].message);
+      res.status(200).json("Welome");
+    } catch (error) {
+      if (error) return alert((error as Error).message);
+    }
   } else {
     res.status(405).json({ message: 'Method Not Allowed' });
   }
