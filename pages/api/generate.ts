a7b36592adc7655689b46e45f55ca2170269ead1
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-05-17 03:50:55 +0000

    u

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -9,23 +9,23 @@ const openai = new OpenAIApi(configuration);
 // method to add two numbers
 
 async function getAIResponse() {
-  
+
   //console.log(completion.data.choices[0].message);
 }
 
 const handler: NextApiHandler = async (req, res) => {
   if (req.method === 'GET') {
-    
+
     const response = {
       role: "",
       content: "here",
     };
-    const completion = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [{role: "user", content: "Hello world"}],
-    });
+    //const completion = 
     //response.content = JSON.stringify({completion});
-    response.role = JSON.stringify({"Assistant":"a"});
+    response.role = JSON.stringify(await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: "Hello world" }],
+    }));
     res.status(200).json(response);
     //getAIResponse();
   } else {
