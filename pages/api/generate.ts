7c732caf94ef03fdbf0e8212cec43a363f09ab38
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-05-17 03:37:35 +0000

    try

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -3,7 +3,7 @@ import { NextApiHandler } from 'next';
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
 // method to add two numbers
@@ -13,18 +13,20 @@ async function getAIResponse() {
   //console.log(completion.data.choices[0].message);
 }
 
-
 const handler: NextApiHandler = async (req, res) => {
   if (req.method === 'GET') {
-    const bot = {
-      name: 'genie',
-      price: 0
+    
+    const response = {
+      role: "",
+      content: "",
     };
     const completion = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: [{role: "user", content: "Hello world"}],
     });
-    res.status(200).json(completion);
+    response.content = JSON.stringify({completion});
+    response.role = JSON.stringify("Assistant");
+    res.status(200).json(response);
     //getAIResponse();
   } else {
     res.status(405).json({ message: 'Method Not Allowed' });
