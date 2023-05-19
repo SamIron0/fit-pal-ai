a5d96e64b37bebf816247f288f05e9a0e42d4cfd
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-05-19 16:22:54 +0000

    d

--- pages/account.tsx
+++ pages/account.tsx
@@ -301,7 +301,7 @@ export default function Account({ user }: { user: User }) {
 
             <div>
               <div className="p-20">
-                <p>{}</p>
+                <p>{responseData.role}</p>
               </div>
               <div className="relative">
                 <input
