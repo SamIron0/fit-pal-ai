9ac851c59b57cd12a0b07ab74c882186fcfabeca
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-05-20 04:48:41 +0000

    hj

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -17,10 +17,10 @@ export default function HomePage() {
 
             <div className="bg-black px-8">
             <div className="rounded-full border h-24 border-gray-600 max-w-md mx-auto sm:max-w-lg">                    
-                    <div className="relative ">
+                    <div className="relative h-full ">
                         <input
                             type="text"
-                            className=" rounded-full py-2 px-4 text-gray-200 w-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+                            className=" rounded-full py-2 h-full px-4 text-gray-200 w-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
                             placeholder="Email address"
                         />
                         <button className="absolute right-0 top-0 h-full px-4 bg-gray-500 text-gray-100 rounded-r-full focus:outline-none hover:bg-gray-600">
