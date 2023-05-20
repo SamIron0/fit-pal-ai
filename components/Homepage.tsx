3e403c7571aa665d654668f19314fa26e53e1bd3
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-05-20 04:22:03 +0000

    n

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -3,7 +3,7 @@ export default function HomePage() {
     return (
         <section className="bg-black">
             <div className="mx-auto py-8 px-4 sm:px-6 lg:px-8">
-                <div className=" h-64 mt-8 sm:align-center ">
+                <div className="  mt-8 sm:align-center ">
                     <p className="text-7xl font-extrabold text-white sm:text-center sm:text-8xl  m-auto">
                         Create. Track. Achieve.
                     </p>
