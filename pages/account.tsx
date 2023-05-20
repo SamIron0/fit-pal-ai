ead835267837e0f2d534f86d0e2ac06db6af70f6
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-05-20 22:10:48 +0000

    x

--- pages/account.tsx
+++ pages/account.tsx
@@ -152,7 +152,7 @@ export default function Account({ user }: { user: User }) {
 
 
   return (
-    <section className="bg-black">
+    <section className="bg-black h-screen">
       <div className="sm:flex px-4 sm:flex-col sm:align-center">
         <div className="border border-zinc-700	max-w-3xl w-full rounded-md m-auto">
           <div className="px-5">
