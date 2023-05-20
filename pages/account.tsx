b367a1ae9070801eeff9f4787c8d0853ed811da6
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-05-20 22:49:53 +0000

    j

--- pages/account.tsx
+++ pages/account.tsx
@@ -326,7 +326,7 @@ export default function Account({ user }: { user: User }) {
               </div>
             </div>
 
-            <div className="border-t fixed bottom-0 border-zinc-700 bg-zinc-900 p-4 text-zinc-500 rounded-b-md">
+            <div className="border-t border-zinc-700 bg-zinc-900 p-4 text-zinc-500 rounded-b-md">
 
               <div >
                 <div className="p-">
