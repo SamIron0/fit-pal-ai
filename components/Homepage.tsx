5e92252557c988fac2bec3f91bdceadd0e269912
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-05-20 03:55:51 +0000

    hj

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -4,7 +4,7 @@ export default function HomePage() {
         <section className="bg-black">
             <div className="mx-auto py-8 px-4 sm:px-6 lg:px-8">
                 <div className=" sm:align-center ">
-                    <p className="text-xl font-extrabold text-white sm:text-center sm:text-8xl max-w-4xl m-auto">
+                    <p className="text-6xl font-extrabold text-white sm:text-center sm:text-8xl max-w-4xl m-auto">
                         Create. Track. Achieve.
                     </p>
                     <h3 className=" text-5xl mt-9 mb-14  text-zinc-200 sm:text-center sm:text-3xl max-w-4xl m-auto">
