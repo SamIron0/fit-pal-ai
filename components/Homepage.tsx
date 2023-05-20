1a4f74aff2271574ff9317e575c97d115640c395
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-05-20 04:36:31 +0000

    hgf

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -16,8 +16,8 @@ export default function HomePage() {
             </div>
 
             <div className="bg-black px-8">
-            <div className="rounded-full border h-20 border-gray-600 max-w-md mx-auto sm:max-w-lg">                    
-                    <div className="relative">
+            <div className="rounded-full border h-19 border-gray-600 max-w-md mx-auto sm:max-w-lg">                    
+                    <div className="relative h-full">
                         <input
                             type="text"
                             className=" rounded-full h-full py-2 px-4 text-gray-200 w-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
