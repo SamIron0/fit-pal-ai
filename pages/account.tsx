cd74e3b0c7e07a2c7e764e9bba2734e7483e1817
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-05-20 21:45:49 +0000

    d

--- pages/account.tsx
+++ pages/account.tsx
@@ -47,9 +47,9 @@ function Card({ title, description, footer, children }: Props) {
 
 function PlanCard({ title, description, footer, children }: Props) {
   return (
-    <div className="border border-zinc-700	max-w-3xl w-full p rounded-md m-auto my-4">
+    <div className="border border-zinc-700	w-l w-full p rounded-md m-auto my-4">
       <div className="px-5 py-2">
-        <h3 className="text-2xl mb-1 font-medium">{title}</h3>
+        <h3 className="text-l mb-1 font-medium">{title}</h3>
         <p className="text-zinc-300">{description}</p>
         {children}
       </div>
