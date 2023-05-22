c20c3344689ad6e83051da83ef57779dd883d413
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-05-22 04:10:14 +0000

    w

--- pages/account.tsx
+++ pages/account.tsx
@@ -127,7 +127,7 @@ export default function Account({ user }: { user: User }) {
 
   const AISection = () => {
     return (
-      <div>
+      <div className="flex-1 overflow-y-scroll">
         {messageList && messageList.map((message, index) => (
           <div className={`${index % 2 === 0 ? 'bg-black' : 'bg'}`}>
             <p className={'mx-5 py-5'}>
