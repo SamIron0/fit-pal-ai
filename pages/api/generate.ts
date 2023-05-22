142b163213dfa3275261c335d66d52824b9c6a8d
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-05-22 23:44:48 +0000

    x

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -46,7 +46,7 @@ const handler: NextApiHandler = async (req, res) => {
         res.status(200).json(message);
         // Handle "delete" response
       } else {
-        res.status(200).json(message);
+        res.status(200).json(AIquery?.toString?.());
       }
     }
     
