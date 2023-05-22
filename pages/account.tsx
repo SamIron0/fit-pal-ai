2c43982770a3f45d5d08031cda8c5a360876c2d1
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-05-22 04:27:08 +0000

    d

--- pages/account.tsx
+++ pages/account.tsx
@@ -172,7 +172,7 @@ export default function Account({ user }: { user: User }) {
   //const section2Class = activeSection === 2 ? "block" : "hidden";
 
   return (
-    <div className="h-screen overflow-hidden">
+    <div className="overflow-hidden">
       <section className="bg-black h-full">
         <div className="sm:flex px-4 sm:flex-col sm:align-center">
           <div className="border border-zinc-700	max-w-3xl w-full rounded-md m-auto">
@@ -374,7 +374,7 @@ export default function Account({ user }: { user: User }) {
                     </div>
                   </div>
 
-                  <div className="mt-2">
+                  <div className="mt-2 bg-white">
                     {activeSection === 1 ? (
                       <AISection />
                     ) : activeSection === 2 ? (
