8d125d4f8e75eab3669aa16c527422f4b2a222bf
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-05-23 05:11:01 +0000

    j

--- pages/account.tsx
+++ pages/account.tsx
@@ -164,7 +164,7 @@ export default function Account({ user }: { user: User }) {
   //const section2Class = activeSection === 2 ? "block" : "hidden";
 
   return (
-    <section className="bg-black flex-auto h-screen" ref={aiSectionRef}>
+    <section className="bg-black flex-auto mt-20 h-screen" ref={aiSectionRef}>
       <div className="sm:flex px-4 sm:flex-col sm:align-center">
         <div className="border border-zinc-700	max-w-3xl w-full rounded-md m-auto">
           <div className="px-3">
@@ -367,7 +367,7 @@ export default function Account({ user }: { user: User }) {
                     </div>
                     <div className=" mx-auto">
                       <div
-                        className={`pt-2 cursor-pointer ${activeSection === 2 ? "border-b border-black" : ""
+                        className={`pt-2 cursor-pointer ${activeSection === 2 ? "border-b-2 border-black" : ""
                           }`}
                         onClick={() => setActiveSection(2)}
                       >
