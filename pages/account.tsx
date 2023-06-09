1965b4e8f114f60b1f988968bf99f9f86f2e6812
sam <ironkwec@myumanitoba.ca>
2023-06-09 00:19:10 -0500

    s

--- pages/account.tsx
+++ pages/account.tsx
@@ -42,7 +42,7 @@ function PlanCard({ title, footer, children, completed }: Props) {
   return (
     <div className={`h-full w-48 p-px rounded-md ${bgColor}`}>
       <div className="bg-black h-full w-full p rounded-md m-auto">
-        <div className='w-48 bg-zinc-700 box-border h-4/5'>
+        <div className='w-48 rounded-t-md bg-zinc-700 box-border h-4/5'>
           <div className="px-2 ">
             <h1 className="text-l  font-medium">{title}</h1>
             {children}
