ffbc8f160a485d26e6a91216e65ae28477a509c1
sam <ironkwec@myumanitoba.ca>
2023-06-11 18:13:50 -0500

    h

--- pages/account.tsx
+++ pages/account.tsx
@@ -419,7 +419,7 @@ export default function Account({ user }: { user: User }) {
 
           <div className="border-t h-96 overflow-hidden border-zinc-700 bg-zinc-900 text-zinc-500 rounded-b-md">
             <div >
-              <div className="h-80 pb-14 text-gray-200">
+              <div className="h-80 pb-11 text-gray-200">
                 <div className="flex flex-col">
                   <div className="flex">
                     <div className=" mx-auto">
