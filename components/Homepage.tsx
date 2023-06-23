06bdf7d246b0389bdd790f10c7676f584c13eee8
sam <ironkwec@myumanitoba.ca>
2023-06-23 16:58:45 -0500

    sd

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -18,7 +18,7 @@ export default function HomePage() {
                     </div>
                     <div>
                         <p className="mt-9 mb-14 text-xl text-zinc-200 sm:text-center sm:text-2xl max-w-4xl m-auto">
-                            Your AI-powered health and fitness companion. Personalized exercises and meal plans, real-time feedback, and support. Achieve your goals with ease.
+                            Your AI-powered health and fitness companion. Personalized, real-time feedback, and support. Achieve your goals with ease.
                         </p>
                     </div>
                 </div>
@@ -30,18 +30,18 @@ export default function HomePage() {
                                 <img src={mealIcon.src} alt="meal image" />
                             </div>
                             <div className="w-3/5">
-                            <input
-                                type="text"
-                                className=" rounded-full w-py-2 h-full px-4 text-gray-200  focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
-                                placeholder="Email address"
-                            />
+                                <input
+                                    type="text"
+                                    className=" rounded-full py-2 h-full px-4 text-gray-200  focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+                                    placeholder="Email address"
+                                />
                             </div>
-                            <div className="w-1/5">
-                            <button className=" h-full px-4  text-gray-100 rounded-r-full focus:outline-none hover:bg-gray-600">
-                                <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 13 5.7-5.326a.909.909 0 0 0 0-1.348L1 1" />
-                                </svg>
-                            </button>
+                            <div className="w-1/5 bg-zinc-500">
+                                <button className=" h-full  text-gray-100 rounded-r-full focus:outline-none hover:bg-gray-600">
+                                    <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 13 5.7-5.326a.909.909 0 0 0 0-1.348L1 1" />
+                                    </svg>
+                                </button>
                             </div>
                         </div>
                     </div>
