6e7e94a8e116ca95f34ec8e6e37e16b9522ba2a5
sam <ironkwec@myumanitoba.ca>
2023-06-24 01:24:42 -0500

    s

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -24,7 +24,7 @@ export default function HomePage() {
                 </div>
 
                 <div className="bg-black pb-24 px-8">
-                    <div className="rounded-full border w-md h-16 border-gray-600 max-w-md mx-auto sm:max-w-lg">
+                    <div className="rounded-full border w-md border-gray-600 max-w-md mx-auto sm:max-w-lg">
                         <div className="flex p-3 w-full">
                             <div className="w-1/4 flex justify-center">
                                 <div className="circle-div"><img src={mealIcon.src} alt="meal image" />
