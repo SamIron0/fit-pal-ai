e49ac993a98f9d9db9a26a856d48d23a4ff77685
sam <ironkwec@myumanitoba.ca>
2023-06-23 22:31:44 -0500

    sd

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -30,7 +30,7 @@ export default function HomePage() {
                                 <div className="circle-div"><img src={mealIcon.src} alt="meal image" />
                                 </div>
                             </div>
-                            <div className="w-11/20 w-full flex justify-center flex-col">
+                            <div className="w-11/20 w-full flex justify-center">
                                 <div className="h-1/2 bg-gray-300">Introducing Fitpal</div>
                                 <div className="h-1/2 bg-gray-400">Row 2</div>
                             </div>
