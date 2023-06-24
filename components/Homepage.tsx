a9b75257cb97061469955956a3c0a3075cdf492b
sam <ironkwec@myumanitoba.ca>
2023-06-24 00:28:39 -0500

    j

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -43,7 +43,7 @@ export default function HomePage() {
                                 </div>
                             </div>
                             <div className="w-1/5 ">
-                                <button className=" h-full  text-gray-100 rounded-r-full focus:outline-none hover:bg-gray-600">
+                                <button className=" h-full pl-4 text-gray-100 rounded-r-full focus:outline-none hover:bg-gray-600">
                                     <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">
                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 13 5.7-5.326a.909.909 0 0 0 0-1.348L1 1" />
                                     </svg>
