901af95829c920d3bd258195f052f0a198f108b0
sam <ironkwec@myumanitoba.ca>
2023-07-12 01:41:51 -0500

    s

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -44,7 +44,7 @@ export default function HomePage() {
                                     </p>
                                 </div>
                             </div>
-                            <Link href="../ai-chatbot/page" className=" flex items-center justify-center w-1/5 ">
+                            <Link href="../ai-chatbot/app/page" className=" flex items-center justify-center w-1/5 ">
                                 <svg className="w-3 h-3 text-white hover:text-blue-500 text-dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 13 5.7-5.326a.909.909 0 0 0 0-1.348L1 1" />
                                 </svg>
