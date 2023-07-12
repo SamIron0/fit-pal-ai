617c132a9c5ca629252e94bfddd2c64113aa8c06
sam <ironkwec@myumanitoba.ca>
2023-07-12 03:10:35 -0500

    h

--- components/button-scroll-to-bottom.tsx
+++ components/button-scroll-to-bottom.tsx
@@ -12,8 +12,6 @@ export function ButtonScrollToBottom({ className, ...props }: ButtonProps) {
 
   return (
     <Button
-      variant="outline"
-      size="icon"
       className={cn(
         'absolute right-4 top-1 z-10 bg-background transition-opacity duration-300 sm:right-8 md:top-2',
         isAtBottom ? 'opacity-0' : 'opacity-100',
