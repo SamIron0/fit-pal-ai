3d557b84ec6b5943c5fc68b2f698454aaa9fc583
sam <ironkwec@myumanitoba.ca>
2023-07-13 01:34:04 -0500

    ju

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -21,7 +21,7 @@ const SignIn = () => {
   }, [user]);
 
   if (!user)
-    return (
+    return ( 
       <div className="flex justify-center height-screen-helper">
         <div className="flex flex-col justify-between max-w-lg p-3 m-auto w-80 ">
           <div className="flex justify-center pb-12 ">
