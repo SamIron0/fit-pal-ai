077bd057f4639b4bd07f0800cfb50095e20c75f0
sam <ironkwec@myumanitoba.ca>
2023-07-13 20:21:46 -0500

    k

--- components/sidebar-list.tsx
+++ components/sidebar-list.tsx
@@ -1,4 +1,4 @@
-import { getChats, removeChat, shareChat } from '@/app/actions'
+import { getChats, removeChat, shareChat } from '@/pages/actions'
 import { SidebarActions } from '@/components/sidebar-actions'
 import { SidebarItem } from '@/components/sidebar-item'
 
